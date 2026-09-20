ba5edca57ab46cd528ef0f9592afce8d0124edc1
Ferdous Ahmed <Ferdoustafe@gmail.com>
2026-09-21 02:10:13 +0600

    files added

---
+++ sourcingforgoods.com/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1768335773;s:7:"authKey";s:64:"OX>n#vf7qBTc<dgB7?5>`dR`2LL~8%hSKn;Y/C%k^]jQU+c+9{?%6a(#1fvZW;jm";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3861;}
